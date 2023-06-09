1fbf8d28e09385c9cf3c7975089dcc666f1e24c9
Remynder0 <remynderyt@gmail.com>
2023-06-09 18:38:17 +0200

    add alphabet button

--- script.js
+++ script.js
@@ -117,6 +117,15 @@ function makeHangman(data) {
     hangman.textContent = sliceWord.join('');
     hangmanBlock.appendChild(hangman);
     hangmanBlock.setAttribute('style', 'display : block');
+    const alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    for (let i = 0; i < alpha.length; i++) {
+        let button = document.createElement('button');
+        button.textContent = alpha[i];
+        hangmanBlock.appendChild(button);
+
+    }
+
+
     document.querySelector("main").appendChild(hangmanBlock);
 }
 
